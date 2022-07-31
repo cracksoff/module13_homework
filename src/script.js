2eb66d37967279bcbff81e78913d1a62326d1cd3
cracksoff <59627202+cracksoff@users.noreply.github.com>
2022-07-31 20:38:21 +0300

    working

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const dir = './assets/doge'
+const dir = './assets/doges/doge'
 
 function photos(dir) {
 	const files = []
@@ -19,5 +19,44 @@ function initSlider() {
 		wrapperImages.insertAdjacentHTML('beforeend', `<img src="${photos(dir)[i]}" alt="img" />`)
 	})
 	wrapperImages.firstChild.classList.add('active')
+
+	arrowSlider()
+}
+
+function arrowSlider() {
+	let wrapperArrows = document.createElement('div')
+	wrapperArrows.className = 'slider-nav'
+	slider.appendChild(wrapperArrows, slider.firstChild)
+
+	let arrowForward = document.createElement('button')
+	arrowForward.className = 'slider-arrow forward'
+	arrowForward.setAttribute('type', 'button')
+	arrowForward.insertAdjacentHTML('beforeend', `<img src="./assets/arrows/arrow-right.svg" alt="img" class="arrow-svg"/>`)
+	wrapperArrows.insertBefore(arrowForward, wrapperArrows.firstChild)
+
+	let arrowBack = document.createElement('button')
+	arrowBack.className = 'slider-arrow back'
+	arrowBack.setAttribute('type', 'button')
+	arrowBack.insertAdjacentHTML('beforeend', `<img src="./assets/arrows/arrow-left.svg" alt="img" class="arrow-svg"/>`)
+	wrapperArrows.insertBefore(arrowBack, wrapperArrows.firstChild)
+
+	let sliderArrows = document.querySelectorAll('.slider-arrow')
+
+	for (let arrow of sliderArrows) {
+		arrow.addEventListener('click', () => {
+			let currentImage = wrapperImages.querySelector('.active')
+
+			let prevImage = currentImage.previousElementSibling || wrapperImages.lastElementChild
+			let nextImage = currentImage.nextElementSibling || wrapperImages.firstElementChild
+
+			currentImage.classList.remove('active')
+
+			if (arrow.classList.contains('back')) {
+				prevImage.classList.add('active')
+			} else {
+				nextImage.classList.add('active')
+			}
+		})
+	}
 }
 initSlider()
